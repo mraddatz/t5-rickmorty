ea3579041f22017ced2ee20b66ee733c086f7478
mraddatz <mraddatz1@uc.cl>
2020-07-10 18:20:44 -0400

    remove prints

--- rickmortyapp/views.py
+++ rickmortyapp/views.py
@@ -22,11 +22,10 @@ def index(request):
     while True:
         response = requests.post(API_URL, json={'query': query.format(page=str(page))})
         response_dic = json.loads(response.text)
-        print("Responde DIC")
-        print(response_dic)
+
         next_page = response_dic['data']['episodes']['info']['next']
         page_results = response_dic['data']['episodes']['results']
-        print(next_page)
+
         for episode in page_results:
             episode_dic = {}
             episode_dic['id'] = episode['id']
@@ -67,8 +66,7 @@ def character(request, character_id):
     response = json.loads(response.text)
     character = response['data']['character']
 
-    print("Characters")
-    print(character)
+
     data = {
         'character': character,
     }
@@ -94,8 +92,7 @@ def search(request):
     
     response_characters = requests.post(API_URL, json={'query': characters_search_query})
     response_characters = json.loads(response_characters.text)
-    print("Response episodes")
-    print(characters_search_query)
+
     try:
         response_characters = response_characters['data']['characters']['results']
     except: 
@@ -105,8 +102,7 @@ def search(request):
     episodes_search_query = """ query {{ episodes ( filter: {{name: "{search_query}" }}) {{ results {{ id name }} }} }}""".format(search_query=search)
     response_episodes = requests.post(API_URL, json={'query': episodes_search_query})
     response_episodes = json.loads(response_episodes.text)
-    print("Response episodes")
-    print(response_episodes)
+
     try:
         response_episodes = response_episodes['data']['episodes']['results']
     except: 
@@ -116,15 +112,11 @@ def search(request):
     location_search_query = """ query {{ locations (filter: {{name: "{search_query}" }}) {{ results {{ id name }} }} }}""".format(search_query=search)
     response_locations = requests.post(API_URL, json={'query': location_search_query})
     response_locations = json.loads(response_locations.text)
-    print("Response location")
-    print(response_locations)
+
     try:
         response_locations = response_locations['data']['locations']['results']
     except: 
         response_locations = []
-    # characters = name_search('character', search_term)
-    # locations = name_search('location', search_term)
-    # episodes = name_search('episode', search_term)
 
     data = {
         'search_query': search,
@@ -138,27 +130,27 @@ def search(request):
     return render(request, 'rickmortyapp/search.html', data)
 
 
-def character_search(search_string):
-    request_link = API_URL + search_type + '/?name=' + search_string
-    response_dic = requests.get(request_link).json()
-    if 'error' in response_dic.keys():
-        return []
-    pages = response_dic['info']['pages']
-    output = []
-    for _ in range(1, pages+1):
-        output += response_dic['results']
-        next_url = response_dic['info']['next']
-        if next_url:
-            response_dic = requests.get(next_url).json()
+# def character_search(search_string):
+#     request_link = API_URL + search_type + '/?name=' + search_string
+#     response_dic = requests.get(request_link).json()
+#     if 'error' in response_dic.keys():
+#         return []
+#     pages = response_dic['info']['pages']
+#     output = []
+#     for _ in range(1, pages+1):
+#         output += response_dic['results']
+#         next_url = response_dic['info']['next']
+#         if next_url:
+#             response_dic = requests.get(next_url).json()
     
-        # characters
-    query = """query {{characters(filter: {{name: "{search_term}" }}) {{results {{id name}}}}}}"""
-    try:
-        r = requests.post(_URL_API, json={'query': query_characters.format(search_term=search_term)})
-        json_data = json.loads(r.text)
-        characters = json_data['data']['characters']['results']
-    except: 
-        characters = [{'id': "",
-                       'name': ""}]
+#         # characters
+#     query = """query {{characters(filter: {{name: "{search_term}" }}) {{results {{id name}}}}}}"""
+#     try:
+#         r = requests.post(_URL_API, json={'query': query_characters.format(search_term=search_term)})
+#         json_data = json.loads(r.text)
+#         characters = json_data['data']['characters']['results']
+#     except: 
+#         characters = [{'id': "",
+#                        'name': ""}]
     
-    return output
+#     return output
