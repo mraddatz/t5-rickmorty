f7843964279c8868b9b5e13c5e5b6a7e233d0d15
mraddatz <mraddatz1@uc.cl>
2020-07-10 20:09:16 -0400

    change url

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('rickmortyapp/', include('rickmortyapp.urls')),
+    path('', include('rickmortyapp.urls')),
     path('admin/', admin.site.urls),
 ]
